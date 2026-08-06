d6b86d5f1f674732f9c0966c5c38b3ee0fa8a5cb
KaneriMio <mikyki.01@gmail.com>
2026-08-06 16:58:52 +0500

    Add files via upload

---
+++ promotion.js
@@ -0,0 +1,34 @@
+const container = document.getElementById("promotionContainer");
+
+fetch("promotion.json")
+    .then(r => r.json())
+    .then(renderPromotions);
+
+function renderPromotions(items){
+
+    container.innerHTML = "";
+
+    items.forEach(item=>{
+
+        const card=document.createElement("div");
+
+        card.className="promotion-card";
+
+        card.innerHTML=`
+            <img src="${item.image}">
+            <h2>${item.name}</h2>
+
+            <div>🐴 ${item.horses} лошадей</div>
+
+            <p>${item.description}</p>
+
+            <div class="promotion-price">
+                ${item.price} ₽
+            </div>
+        `;
+
+        container.appendChild(card);
+
+    });
+
+}ы
